27407f558c0604c84d03d6a16cee129738d161cd
AliReza <ali.reza.buet@gmail.com>
2023-11-28 03:13:14 +0600

    Update the buy and category model

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,6 +1,7 @@
 class Buy < ApplicationRecord
-    belongs_to :category
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+    belongs_to :author, class_name: 'User'
+    has_many :buys_categories
+    has_many :categories, through: :buys_categories
     validates :name, presence: true
     validates :amount, presence: true
 end
