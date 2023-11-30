522fc182f3b2348486a9e63d627bc097ad1cda0b
AliReza <ali.reza.buet@gmail.com>
2023-11-30 14:41:19 +0600

    Add integration test for transactions

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
     has_many :buys_categories
     has_many :buys, through: :buys_categories
     validates :name, presence: true
+    validates :icon, presence: true
 end
